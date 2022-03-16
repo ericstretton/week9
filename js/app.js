874b7bc8e99df2c1dc4ef0961922ec7b4bea5065
ericstretton <eric.stretton@gmail.com>
2022-03-16 17:51:37 -0600

    Edits after reviewing script with Mark.

--- js/app.js
+++ js/app.js
@@ -10,46 +10,7 @@ if (userAge < 18 && is_subscribed == false){
     console.log("Limited Access Granted, Welcome!");
 }else if(userAge >= 18 && is_subscribed ==true){
     console.log("Welcome!");
+}else {
+    console.log("Error");
 }
 
-var userAge = 17;
-
-var is_subscribed = false;
-
-if (userAge < 18 && is_subscribed == false){
-    console.log("Go away!");
-}else if((userAge >= 18) && (is_subscribed ==false)){
-    console.log("Welcome, Please Subscribe!");
-}else if((userAge < 18) && (is_subscribed ==true)){
-    console.log("Limited Access Granted, Welcome!");
-}else if(userAge >= 18 && is_subscribed ==true){
-    console.log("Welcome!");
-}
-
-var userAge = 25;
-
-var is_subscribed = false;
-
-if (userAge < 18 && is_subscribed == false){
-    console.log("Go away!");
-}else if((userAge >= 18) && (is_subscribed ==false)){
-    console.log("Welcome, Please Subscribe!");
-}else if((userAge < 18) && (is_subscribed ==true)){
-    console.log("Limited Access Granted, Welcome!");
-}else if(userAge >= 18 && is_subscribed ==true){
-    console.log("Welcome!");
-}
-
-var userAge = 25;
-
-var is_subscribed = true;
-
-if (userAge < 18 && is_subscribed == false){
-    console.log("Go away!");
-}else if((userAge >= 18) && (is_subscribed ==false)){
-    console.log("Welcome, Please Subscribe!");
-}else if((userAge < 18) && (is_subscribed ==true)){
-    console.log("Limited Access Granted, Welcome!");
-}else if(userAge >= 18 && is_subscribed ==true){
-    console.log("Welcome!");
-}
