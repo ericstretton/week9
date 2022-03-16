a625492b2e3a545bea32b1d18dea27d3ee3409cc
ericstretton <eric.stretton@gmail.com>
2022-03-16 13:35:04 -0600

    Week nine assignment A is working, I plan to check with mark if it is correctly answering the assignments problem.

---
+++ js/app.js
@@ -0,0 +1,55 @@
+var userAge = 17;
+
+var is_subscribed = true;
+
+if (userAge < 18 && is_subscribed == false){
+    console.log("Go away!");
+}else if((userAge >= 18) && (is_subscribed ==false)){
+    console.log("Welcome, Please Subscribe!");
+}else if((userAge < 18) && (is_subscribed ==true)){
+    console.log("Limited Access Granted, Welcome!");
+}else if(userAge >= 18 && is_subscribed ==true){
+    console.log("Welcome!");
+}
+
+var userAge = 17;
+
+var is_subscribed = false;
+
+if (userAge < 18 && is_subscribed == false){
+    console.log("Go away!");
+}else if((userAge >= 18) && (is_subscribed ==false)){
+    console.log("Welcome, Please Subscribe!");
+}else if((userAge < 18) && (is_subscribed ==true)){
+    console.log("Limited Access Granted, Welcome!");
+}else if(userAge >= 18 && is_subscribed ==true){
+    console.log("Welcome!");
+}
+
+var userAge = 25;
+
+var is_subscribed = false;
+
+if (userAge < 18 && is_subscribed == false){
+    console.log("Go away!");
+}else if((userAge >= 18) && (is_subscribed ==false)){
+    console.log("Welcome, Please Subscribe!");
+}else if((userAge < 18) && (is_subscribed ==true)){
+    console.log("Limited Access Granted, Welcome!");
+}else if(userAge >= 18 && is_subscribed ==true){
+    console.log("Welcome!");
+}
+
+var userAge = 25;
+
+var is_subscribed = true;
+
+if (userAge < 18 && is_subscribed == false){
+    console.log("Go away!");
+}else if((userAge >= 18) && (is_subscribed ==false)){
+    console.log("Welcome, Please Subscribe!");
+}else if((userAge < 18) && (is_subscribed ==true)){
+    console.log("Limited Access Granted, Welcome!");
+}else if(userAge >= 18 && is_subscribed ==true){
+    console.log("Welcome!");
+}
